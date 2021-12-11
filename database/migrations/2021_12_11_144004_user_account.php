0a08473391b4cc86d0589b19a9271f94ac13e268
GunaRakulan-GR <gunarakulan@gmail.com>
2021-12-11 20:41:01 +0530

    Write database migration code

--- database/migrations/2021_12_11_144004_user_account.php
+++ database/migrations/2021_12_11_144004_user_account.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePasswordResetsTable extends Migration
+class UserAccount extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,17 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
-        });
+      Schema::create('user_account', function (Blueprint $table) {
+
+        $table->increments('auto_id');
+        $table->string('staff_id');
+        $table->string('username');
+        $table->string('password');
+        $table->string('account_type');
+
+      });
+
+
     }
 
     /**
@@ -27,6 +33,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        //
     }
 }
