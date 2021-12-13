c11a36684725019b527b8b3aee4c4631ad1bd9b3
GunaRakulan-GR <gunarakulan@gmail.com>
2021-12-13 16:34:07 +0530

    Get staff_id and firstname+lastname to the staff_id in user account creation

--- resources/views/admin-dashboard-content/user-accounts-page-1-index.blade.php
+++ resources/views/admin-dashboard-content/user-accounts-page-1-index.blade.php
@@ -61,16 +61,16 @@
         <div class="form-group row">
           <label for="username" class="col-sm-2 col-form-label">Staff ID</label>
           <div class="col-sm-8">
-            <select class="form-control" aria-label="Default select example">
+            <select class="form-control" name = "staff_id" id="staff_id" aria-label="Default select example">
+
               <option selected>Select a staff</option>
-              <option value="1">One</option>
-              <option value="2">Two</option>
-              <option value="3">Three</option>
+              @foreach ($staff_data as $key => $data)
+                <option value="{{$data->staff_id}}">{{$data->staff_id}} ({{$data->firstname}} {{$data->lastname}})</option>
+              @endforeach
+
             </select>
           </div>
         </div>
-
-
 
         <div class="form-group row">
           <label for="username" class="col-sm-2 col-form-label">Username</label>
